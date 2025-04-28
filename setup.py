96524721914633c9e42621b6abeb0c949fb6bba4
pranav <pranav.pol@flex.com>
2025-04-28 17:47:32 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name ='Generative AI Project',
+    version='0.0.0',
+    author='pranavpol',
+    author_email='pranav.pol@flex.com',
+    packages=find_packages(),
+    install_requires = [] 
+)
